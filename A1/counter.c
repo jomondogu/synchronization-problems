ce7179425d919d18064eb853cd95792c37388b0a
Matt Stewart <themattstewart@gmail.com>
2018-10-09 14:51:38 -0700

    add dining philosophers

--- A1/counter.c
+++ A1/counter.c
@@ -16,7 +16,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, see http://www.gnu.org/licenses/gpl.html
-    or write to the Free Software Foundation, Inc., 51 Franklin St, 
+    or write to the Free Software Foundation, Inc., 51 Franklin St,
     Fifth Floor, Boston, MA  02110-1301  USA
 */
 
